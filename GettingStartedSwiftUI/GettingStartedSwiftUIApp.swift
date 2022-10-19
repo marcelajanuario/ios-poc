0a7d33f754dd40a13ed5e98dbc87d41962a65be3
mjanuario <marcelajanuario92@hotmail.com>
2022-10-19 18:53:54 -0300

    Criação do projeto

---
+++ GettingStartedSwiftUI/GettingStartedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedSwiftUIApp.swift
+//  GettingStartedSwiftUI
+//
+//  Created by mjanuario on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
